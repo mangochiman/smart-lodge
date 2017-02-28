b87b8cfa68a2691a588adb3113724679b6caface
mangochiman <mangochiman@gmail.com>
2017-02-28 21:09:55 +0200

    Invoice

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -65,7 +65,8 @@ class PagesController < ApplicationController
     @room = Booking.room(params[:booking_id])
     @person = Booking.find(params[:booking_id]).person
     @page_title = "Invoice For  #{@person.first_name} #{@person.last_name} - Room #: <a>#{@room.number}</a>, Room Name: <a>#{@room.name}</a>"
-
+    @booking = Booking.find(params[:booking_id])
+    @billable_items = @booking.billable_items
     @check_in_date = Booking.check_in_date(params[:booking_id])
     @total_days_spent = (Date.today - @check_in_date.to_date).to_i
   end
